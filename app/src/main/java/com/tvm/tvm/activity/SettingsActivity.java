07fc20d8b04b4cb469b95a07b0bf1725480a4df0
jat-chun <919700667@qq.com>
2018-12-13 22:11:10 +0800

    Update For Setting page

--- app/src/main/java/com/tvm/tvm/activity/SettingsActivity.java
+++ app/src/main/java/com/tvm/tvm/activity/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.tvm.tvm.activity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.tvm.tvm.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -13,6 +16,25 @@ import butterknife.ButterKnife;
  * - @Time： 21:23
  */
 public class SettingsActivity extends BaseActivity {
+
+    @BindView(R.id.ib_settings_back)
+    ImageButton iv_settings_back;
+    @BindView(R.id.tv_settings_person_manage)
+    TextView tv_settings_person_manage;
+    @BindView(R.id.tv_settings_normal)
+    TextView tv_settings_normal;
+    @BindView(R.id.tv_settings_ticket)
+    TextView tv_settings_ticket;
+    @BindView(R.id.tv_settings_bill)
+    TextView tv_settings_bill;
+    @BindView(R.id.tv_settings_bill_query)
+    TextView tv_settings_bill_query;
+    @BindView(R.id.tv_settings_exception_query)
+    TextView tv_settings_exception_query;
+    @BindView(R.id.tv_settings_about)
+    TextView tv_settings_about;
+    @BindView(R.id.tv_setting_disclaimer)
+    TextView tv_setting_disclaimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

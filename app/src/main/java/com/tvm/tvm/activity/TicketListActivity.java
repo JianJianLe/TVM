73cdae0cbb482581a326e8002aa27a221215c095
jat-chun <919700667@qq.com>
2019-01-08 22:18:59 +0800

    Update For 票价

--- app/src/main/java/com/tvm/tvm/activity/TicketListActivity.java
+++ app/src/main/java/com/tvm/tvm/activity/TicketListActivity.java
@@ -1,15 +1,24 @@
 package com.tvm.tvm.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.tvm.tvm.R;
+import com.tvm.tvm.adapter.TicketListAdapter;
+import com.tvm.tvm.application.AppApplication;
+import com.tvm.tvm.bean.PriceList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * - @Description:  $desc$
@@ -22,19 +31,45 @@ public class TicketListActivity extends BaseActivity {
     @BindView(R.id.rv_ticket_list_list)
     RecyclerView rv_ticket_list_list;
 
+    @BindView(R.id.ib_ticket_list_add)
+    ImageButton ib_ticket_list_add;
+
+    @BindView(R.id.ib_ticket_list_back)
+    ImageButton ib_ticket_list_back;
+
+    private List<PriceList> priceLists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_list);
         ButterKnife.bind(this);
+        getList();
+        initLayout();
     }
 
+    public void getList(){
+        priceLists = AppApplication.getApplication().getDaoSession().getPriceListDao().queryBuilder().list();
+    }
 
     private void initLayout(){
         GridLayoutManager layoutManager = new GridLayoutManager(this,3);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         rv_ticket_list_list.setLayoutManager(layoutManager);
-
+        rv_ticket_list_list.setAdapter(new TicketListAdapter(this,priceLists));
+        rv_ticket_list_list.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        rv_ticket_list_list.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
     }
 
+    @OnClick({R.id.ib_ticket_list_add,R.id.ib_ticket_list_back})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.ib_ticket_list_back:
+                this.finish();
+                break;
+            case R.id.ib_ticket_list_add:
+                startActivity(this,TicketEditActivity.class);
+                break;
+        }
+    }
 }
--- app/src/main/java/com/tvm/tvm/adapter/TicketListAdapter.java
+++ app/src/main/java/com/tvm/tvm/adapter/TicketListAdapter.java
@@ -1,9 +1,21 @@
 package com.tvm.tvm.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tvm.tvm.R;
+import com.tvm.tvm.activity.TicketEditActivity;
+import com.tvm.tvm.bean.PriceList;
+
+import java.util.List;
 
 /**
  * - @Description:  $desc$
@@ -13,10 +25,28 @@ import android.view.ViewGroup;
  */
 public class TicketListAdapter extends RecyclerView.Adapter {
 
+    private Context context;
+
+    List<PriceList> priceLists;
+
+    public TicketListAdapter(Context context,List<PriceList> priceLists){
+        this.context = context;
+        this.priceLists = priceLists;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View v = LayoutInflater.from(context).inflate(R.layout.item_ticket,null);
+        return new TicketItemViewHolder(v, new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                PriceList item = priceLists.get(position+1);
+                Intent intent = new Intent(context,TicketEditActivity.class);
+                intent.putExtra("id",item.getId());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -26,14 +56,37 @@ public class TicketListAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 15;
     }
 
-    class TicketItemViewHolder extends RecyclerView.ViewHolder{
+    class TicketItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TicketItemViewHolder(@NonNull View itemView) {
+        private TextView tv_item_ticket_title;
+
+        private TextView tv_item_ticket_price;
+
+        private TextView tv_item_ticket_desc;
+
+        private ImageView iv_item_ticket_icon;
+
+        private OnItemClickListener onItemClickListener;
+
+        public TicketItemViewHolder(@NonNull View itemView,OnItemClickListener onItemClickListener) {
             super(itemView);
+            tv_item_ticket_desc = itemView.findViewById(R.id.tv_item_ticket_desc);
+            tv_item_ticket_price = itemView.findViewById(R.id.tv_item_ticket_price);
+            tv_item_ticket_title = itemView.findViewById(R.id.tv_item_ticket_title);
+            iv_item_ticket_icon = itemView.findViewById(R.id.iv_item_ticket_icon);
+            this.onItemClickListener = onItemClickListener;
         }
 
+        @Override
+        public void onClick(View v) {
+            onItemClickListener.onItemClick(v,getPosition());
+        }
+    }
+
+    public  static interface OnItemClickListener{
+        void onItemClick(View view,int position);
     }
 }

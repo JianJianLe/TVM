bbd6a407f706df5fcbb9be3827daf7be22aa7b13
jat-chun <919700667@qq.com>
2019-01-16 23:02:17 +0800

    Update For activity

--- app/src/main/java/com/tvm/tvm/activity/PayDetailActivity.java
+++ app/src/main/java/com/tvm/tvm/activity/PayDetailActivity.java
@@ -2,10 +2,18 @@ package com.tvm.tvm.activity;
 
 import android.os.Bundle;
 
+import com.tvm.tvm.R;
+
+import butterknife.ButterKnife;
+
 public class PayDetailActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_detail);
+        ButterKnife.bind(this);
     }
+
+
 }

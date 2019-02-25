16da59c2d1f1c4a168cf1dc934dd037854b87687
chenchunjie <chencj30188@hsyuntai.com>
2019-02-25 20:38:22 +0800

    Update

---
+++ app/src/main/java/com/tvm/tvm/activity/BillSettingListActivity.java
@@ -0,0 +1,11 @@
+package com.tvm.tvm.activity;
+
+import android.os.Bundle;
+
+public class BillSettingListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

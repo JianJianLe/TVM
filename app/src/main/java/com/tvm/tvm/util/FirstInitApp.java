435c45f91d91e0ba3491b2d7656d8d800757d7e5
jat-chun <919700667@qq.com>
2019-02-28 23:02:10 +0800

    Update For 去除票据初始化

--- app/src/main/java/com/tvm/tvm/util/FirstInitApp.java
+++ app/src/main/java/com/tvm/tvm/util/FirstInitApp.java
@@ -42,7 +42,7 @@ public class FirstInitApp {
             initUser();
             initNormalSetting();
             addData();
-            initBillSettings();
+//            initBillSettings();
         }
 
     }
